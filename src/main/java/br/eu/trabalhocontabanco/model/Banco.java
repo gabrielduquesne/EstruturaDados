316c63b1739d5fc9008425ec28e7e49fa7632884
gabrielduquesne <gabriel.reolon@edu.unipar.br>
2023-06-08 13:24:32 -0300

    criação da classe banco

---
+++ src/main/java/br/eu/trabalhocontabanco/model/Banco.java
@@ -0,0 +1,9 @@
+package br.eu.trabalhocontabanco.model;
+
+
+public class Banco {
+     private int numeroConta;
+    private String nomeTitular;
+    private double saldo;
+    private double deposito;
+    private double saque;
